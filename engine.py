1a1f68e445d50b6db4195693ce1b34b9fb8126c0
Jax Davis <jaxdavis@dhcp-10-105-97-68.wireless.northwestern.private>
2015-11-21 14:42:37 -0600

    Twilio working

--- engine.py
+++ engine.py
@@ -1,12 +1,15 @@
 from twilio.rest import TwilioRestClient 
+from secrets import TWILIO_SID
+from secrets import TWILIO_AUTH
  
-ACCOUNT_SID = "its a secret huehuehue" 
-AUTH_TOKEN = "what part of secret do you not understand" 
+ACCOUNT_SID = TWILIO_SID 
+AUTH_TOKEN = TWILIO_AUTH
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
- 
-client.messages.create(
-    to="+16304701481", 
-    from_="+16305280456", 
-    body="Someone entered!",  
-)
+
+def sendAlert(): 
+	client.messages.create(
+	    to="+16304701481", 
+	    from_="+16305280456", 
+	    body="Someone entered!",  
+	)
--- main.py
+++ main.py
@@ -10,5 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def testSend():
+	engine.sendAlert()
+	return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
