68c0ad47875cc2d9ef7413ff0d298e00326b13c9
Jax Davis <jaxdavis@dhcp-10-105-97-68.wireless.northwestern.private>
2015-11-21 13:53:35 -0600

    Added flask

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, flash, jsonify, render_template, redirect
+from flask_bootstrap import Bootstrap
+import engine
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/home')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
