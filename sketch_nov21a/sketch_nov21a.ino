251248033313344bbb06a4612727cdab19a00c13
Daniel Margosian <margosi2@illinois.edu>
2015-11-21 22:12:43 -0600

    added some cool color stuff

--- sketch_nov21a/sketch_nov21a.ino
+++ sketch_nov21a/sketch_nov21a.ino
@@ -3,25 +3,18 @@
 #include <Wire.h>
 
 const int buttonPin = 2;
-
 int buttonState = 0;
 
 rgb_lcd lcd;
 
-const int colorR = 255;
-const int colorG = 0;
-const int colorB = 0;
-
-String message = "";   // for incoming serial data
-
 void setup() {
   Serial.begin(115200);
+
   Serial.write('1');
 
   lcd.begin(16, 2);
-  lcd.setRGB(colorR, colorG, colorB);
+  lcd.setRGB(255, 0, 0);
   lcd.print("Hello");
-
   pinMode(buttonPin, INPUT);
   Serial.write('1');
 }
@@ -33,15 +26,15 @@ void loop() {
   if (buttonState == HIGH & !rang) {
     Serial.write('0');
     lcd.clear();
-    lcd.print("waiting for message");
-    while (Serial.available() < 0)
-    {
-    }
+    lcd.setRGB(255, 255, 0);
+    lcd.print("Please wait");
+    delay(100);
     while (str == "")
     {
       str = Serial.readString();
     }
     lcd.clear();
+    lcd.setRGB(0, 255, 0);
     lcd.print(str);
   }
 }
